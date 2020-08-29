52e77bce93ddaae74cd816d85f1a16a059c95923
mcgillshakore <mcgills2093@gmail.com>
2020-08-29 12:45:24 -0500

    updated backend v1.4

--- backend-api/app/controllers/api/v1/auth_controller.rb
+++ backend-api/app/controllers/api/v1/auth_controller.rb
@@ -1,5 +1,6 @@
 class AuthController < ApplicationController
-    def login
+    def signin
+        byebug
         user = User.find_by({ username: params[:username]})
         if(user && user.authenticate(params[:password]))
             session[:user_id] = user.id
@@ -7,5 +8,19 @@ class AuthController < ApplicationController
         else
             render json: { success: false, id: nil }
         end
+    end
+
+    def check_signin
+        if session[:user_id]
+            user = User.find(session[:user_id])
+            render json: {username:user.username, id:user.id, age:user.age}
+        else
+            render json: {error:'No one is logged in'}
+        end
+    end
+
+    def signout
+        session[:user_id] = nil
+        render json: {error:'Logged out'}
     end
 end
