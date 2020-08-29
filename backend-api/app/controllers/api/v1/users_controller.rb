feb1ff85ca84b3d63165d522b331172bd31d3061
mcgillshakore <mcgills2093@gmail.com>
2020-08-29 12:25:34 -0500

    updated backend v1.3

--- backend-api/app/controllers/api/v1/users_controller.rb
+++ backend-api/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
     module V1
         class UsersController < ApplicationController
+
             def index
                 users = User.all
                 render json: UserSerializer.new(users).serialized_json
@@ -8,18 +9,31 @@ module Api
 
             def create
                 user = User.create(user_params)
-                byebug
                 if user.save
-                    render json: {user: UserSerializer.new(user)}.serialized_json
+                    render json: {user: UserSerializer.new(user)}
                 else
                     render json: {error: user.errors.messages}, status: 422
                 end
+            end
+
+            def show
+                user = User.find_by(id: params[:id])
+                render json: UserSerializer.new(user).serialized_json
             end
 
             def update
                 user = User.find_by(id: params[:id])
                 if user.update(user_params)
                     render json: UserSerializer.new(user).serialized_json
+                else
+                    render json: {error: user.errors.messages}, status: 422
+                end
+            end
+
+            def destroy
+                user = User.find_by(id: params[:id])
+                if user.destroy
+                    head :no_content
                 else
                     render json: {error: user.errors.messages}, status: 422
                 end
